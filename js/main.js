93ff0666e952a8b991c8d8d759c073c1058c26e5
mehediemon007 <mehediemon7@gmail.com>
2021-11-09 10:49:52 +0600

    selectbox

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * ----------------------------------------------------------
+ *
+ * Template : AmerSolution
+ * Author : AkaatIt
+ * Author URI : https://akaarit.com/
+ *
+ * ----------------------------------------------------------
+ *
+ **/
+
+;(function($){
+
+    'use strict'
+
+    $(function(){
+        $('.selectpicker').selectpicker();
+    });
+
+})(jQuery);
