266725a6eecf4d8008d1bbc93e0adc5872bcec5c
mehediemon007 <mehediemon7@gmail.com>
2021-11-15 18:31:12 +0600

    formatting

--- js/main.js
+++ js/main.js
@@ -39,15 +39,15 @@
 
 })(jQuery);
 
-
-   function langSelect(){
-       document.getElementById("lang-dropdown").classList.toggle("show");
-   }
-
+(function(){
+    function langSelect(){
+        document.getElementById("lang-dropdown").classList.toggle("show");
+    }
+ 
     function actionSelect() {
         document.getElementById("action-dropdown").classList.toggle("show");
     }
-    
+     
     window.onclick = function(event) {
         if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -60,50 +60,52 @@
         }
         }
     }
-
+ 
     // Text Editor
-
+ 
     CKEDITOR.replace( 'textbox' );
-
+ 
     // Select Box
-
-    const selected = document.querySelector(".selected");
-    const optionsContainer = document.querySelector(".options-container");
-    const searchBox = document.querySelector(".search-box input");
-
-    const optionsList = document.querySelectorAll(".option");
-
+ 
+     const selected = document.querySelector(".selected");
+     const optionsContainer = document.querySelector(".options-container");
+     const searchBox = document.querySelector(".search-box input");
+ 
+     const optionsList = document.querySelectorAll(".option");
+ 
     selected.addEventListener("click", () => {
-    optionsContainer.classList.toggle("active");
-
-    searchBox.value = "";
-    filterList("");
-
-    if (optionsContainer.classList.contains("active")) {
-        searchBox.focus();
-    }
-    });
-
-    optionsList.forEach(o => {
-    o.addEventListener("click", () => {
-        selected.innerHTML = o.querySelector("label").innerHTML;
-        optionsContainer.classList.remove("active");
-    });
-    });
-
-    searchBox.addEventListener("keyup", function(e) {
-    filterList(e.target.value);
-    });
-
-    const filterList = searchTerm => {
-    searchTerm = searchTerm.toLowerCase();
-    optionsList.forEach(option => {
-        let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
-        if (label.indexOf(searchTerm) != -1) {
-        option.style.display = "block";
-        } else {
-        option.style.display = "none";
+        optionsContainer.classList.toggle("active");
+ 
+        searchBox.value = "";
+        filterList("");
+ 
+        if (optionsContainer.classList.contains("active")) {
+            searchBox.focus();
         }
     });
+ 
+    optionsList.forEach(o => {
+        o.addEventListener("click", () => {
+            selected.innerHTML = o.querySelector("label").innerHTML;
+            optionsContainer.classList.remove("active");
+        });
+    });
+ 
+    searchBox.addEventListener("keyup", function(e) {
+        filterList(e.target.value);
+    });
+ 
+    const filterList = searchTerm => {
+        searchTerm = searchTerm.toLowerCase();
+        optionsList.forEach(option => {
+            let label = option.firstElementChild.nextElementSibling.innerText.toLowerCase();
+            if (label.indexOf(searchTerm) != -1) {
+            option.style.display = "block";
+            } else {
+            option.style.display = "none";
+            }
+        });
     };
+})()
+   
 
