093d6f428f7225f0e0bd9e554736cc5b483f4be2
mehediemon007 <mehediemon7@gmail.com>
2022-05-31 10:20:17 +0600

    date picker

--- js/main.js
+++ js/main.js
@@ -68,6 +68,19 @@
         
     }
 
+    // Range Date Picker
+
+    $(function() {
+        $('input[name="datetimes"]').daterangepicker({
+          timePicker: true,
+          startDate: moment().startOf('hour'),
+          endDate: moment().startOf('hour').add(32, 'hour'),
+          locale: {
+            format: 'M/DD hh:mm A'
+          }
+        });
+    });
+    
 })(jQuery);
 
 function langSelect(){
